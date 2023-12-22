4325e102e6f786a2c5fde69fc89e28ad33705ffc
sukhvinder-jaat <monudhaka829@gmail.com>
2023-12-22 16:17:54 +0530

    modals file done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./views/Main";
 import BlogPage from "./views/BlogPage";
 import BackToTopButton from "./common/BackToTopButton";
 import Preloader from "./common/Preloader";
-import { useEffect, useState } from "react";
 import AboutPage from "./views/AboutPage";
 import ContactPage from "./views/ContactPage";
 
 function App() {
   const [loading, setLoading] = useState(true);
+
   // FOR FULL SCREEN LOADING
   useEffect(() => {
     document.body.classList.add("overflow-hidden-while-loading");
@@ -18,10 +19,11 @@ function App() {
       document.body.classList.remove("overflow-hidden-while-loading");
     }, 2000);
   }, []);
+
   return (
     <>
       <BackToTopButton />
-      {/* {loading && <Preloader />} */}
+      {loading && <Preloader />}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/blog-page" element={<BlogPage />} />
